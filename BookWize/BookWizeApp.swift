c0fa73dbf93531dc83119f63040e6cb941a398a9
Aryansingh0810 <ss0854850@gmail.com>
2025-03-17 21:10:24 +0530

    Initial Commit

---
+++ BookWize/BookWizeApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookWizeApp.swift
+//  BookWize
+//
+//  Created by Aryan Singh on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BookWizeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
